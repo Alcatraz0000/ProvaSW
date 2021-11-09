766da40cef9f4498c22f1832c0200b4719fe410f
alcatraz <ferdisica@icloud.com>
2021-11-09 17:20:28 +0100

    Program already test

--- ProvaSW/src/provasw/ProvaSW.java
+++ ProvaSW/src/provasw/ProvaSW.java
@@ -17,6 +17,7 @@ public class ProvaSW {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Word!");
+        System.out.println("Hello Universe!");
     }
     
     
